9ddf61cab9f823a234750a6fc403290aafd2cc9b
guilherme-zobel <guilherme.zobel@gmail.com>
2022-05-30 09:42:41 -0300

    feat: setup Front-end

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cadastro de Usuários MERN</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const routes =  require('./src/routes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 mongoose.connect('mongodb://localhost:27017/curso-basico-mern',{
     useUnifiedTopology:true,
