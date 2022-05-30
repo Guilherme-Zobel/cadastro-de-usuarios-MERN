58a96c9df4fce0dd15bea9db6e2e88890c564b46
guilherme-zobel <guilherme.zobel@gmail.com>
2022-05-30 10:09:43 -0300

    feat: setup Material-UI React

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Inicio from './pages/client/painel'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <h1>Cadastro de Usuários MERN</h1>
-      </header>
+      </header> */}
+      <Inicio />
     </div>
   );
 }
